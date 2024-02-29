572a287b4748f6f8d51d8eb7a110031375b7e3e9
Jon Bryant <jonathanwbryant@gmail.com>
2024-02-28 16:09:55 -0800

    Update greeting_program.py

--- greeting_program.py
+++ greeting_program.py
@@ -1,19 +1,21 @@
-# Get the user's name by filling in the code
-name = 
-
-# Get the current time of day
-# We'll assume morning is from 5:00 AM to 11:59 AM, afternoon is from 12:00 PM to 4:59 PM, and evening is from 5:00 PM to 4:59 AM
 import datetime
-current_time = datetime.datetime.now().time()
-hour = current_time.hour
 
-# Determine the appropriate greeting by filling in the correct text
-if 5 <= hour < 12:
-    greeting = 
-elif 12 <= hour < 17:
-    greeting = 
-else:
-    greeting = 
 
-# Print the personalized greeting by finishing the print statement
-print()
+def get_part_of_day(hour):
+    if 5 <= hour < 12:
+        return 'morning'
+    elif 12 <= hour < 17:
+        return 'afternoon'
+    elif 17 <= hour < 21:
+        return 'evening'
+    else:
+        return 'night'
+
+name = input("What is your name? ")
+
+current_time = datetime.datetime.now(12:45)
+part_of_day = get_part_of_day(current_time.hour)
+
+# Greet the user
+
+print (f"Good {part_of_day},{name}")
